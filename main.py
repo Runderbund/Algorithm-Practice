7c801ea6f98be769bdec1fa9b4cd25c9c6480af2
unknown <sean.herbison@gmail.com>
2023-10-08 19:04:40 -0700

    Completed reverse_recursive funtion.

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ def print_duplicates(string):
             print(f"{char}: {count} times")
     else:
         print("No duplicate characters found in the string.")
-
-
 print_duplicates("Hello World!")
 print_duplicates("Hello, hello, World!")
+
+def reverse_recursive(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse_recursive(string[1:]) + string[0]
+print(reverse_recursive("Hello World!"))
+print(reverse_recursive("Hello, hello, World!"))
