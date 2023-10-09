b0e8f7b17b19ce9a0304b485c62126066e15e7ef
unknown <sean.herbison@gmail.com>
2023-10-08 19:06:11 -0700

    Completed count_vowels_consonants function.

--- main.py
+++ main.py
@@ -27,3 +27,17 @@ def reverse_recursive(string):
         return reverse_recursive(string[1:]) + string[0]
 print(reverse_recursive("Hello World!"))
 print(reverse_recursive("Hello, hello, World!"))
+
+def count_vowels_consonants(string):
+    vowels = "aeiou"
+    vowel_count = 0
+    consonant_count = 0
+    for char in string.lower():
+        if char.isalpha():
+            if char in vowels:
+                vowel_count += 1
+            else:
+                consonant_count += 1
+    return {"vowels": vowel_count, "consonants": consonant_count}
+print(count_vowels_consonants("Hello World!"))
+print(count_vowels_consonants("The quick brown fox jumps over the lazy dog."))
